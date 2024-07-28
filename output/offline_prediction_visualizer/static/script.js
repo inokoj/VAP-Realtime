63d7a6cf9d410dd213d60b64e8baf749f1cc64c1
Saeki-M <saeki@pcl.cs.waseda.jp>
2024-07-28 12:13:44 +0900

    updated UI to mkae it closer to original visualizer

--- output/offline_prediction_visualizer/static/script.js
+++ output/offline_prediction_visualizer/static/script.js
@@ -11,14 +11,11 @@ document.addEventListener('DOMContentLoaded', function () {
         return wavesurfer;
     };
 
-    const wavesurferLeft = initializeWaveSurfer('#waveform_left', 'violet', 'purple', '/audio/left');
-    const wavesurferRight = initializeWaveSurfer('#waveform_right', 'lightblue', 'blue', '/audio/right');
+    const wavesurferLeft = initializeWaveSurfer('#waveform_left', 'rgb(255, 165, 0)', 'rgba(255, 165, 0, 0.5)', '/audio/left');
+    const wavesurferRight = initializeWaveSurfer('#waveform_right', 'rgb(0, 0, 255)', 'rgba(0, 0, 255, 0.5)', '/audio/right');
 
     const synchronizePlayPause = (play) => {
         if (play) {
-            const currentTime = wavesurferLeft.getCurrentTime();
-            const duration = wavesurferRight.getDuration();
-            wavesurferRight.seekTo(currentTime / duration);
             wavesurferLeft.play();
             wavesurferRight.play();
         } else {
@@ -49,7 +46,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     document.addEventListener('keydown', (event) => {
-        switch(event.code) {
+        switch (event.code) {
             case 'Space':
                 event.preventDefault();
                 togglePlayPause();
@@ -65,104 +62,116 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    const synchronizeSeek = (wavesurferFrom, wavesurferTo) => {
-        const currentTime = wavesurferFrom.getCurrentTime();
-        const duration = wavesurferTo.getDuration();
-        const isPlaying = wavesurferFrom.isPlaying();
-
-        wavesurferTo.pause();
-        wavesurferTo.seekTo(currentTime / duration);
-        if (isPlaying) {
-            setTimeout(() => wavesurferTo.play(), 0); // Small timeout to ensure the seek is processed first
-        }
-    };
-
     fetch('/data')
         .then(response => response.json())
         .then(data => {
-            const time = data.map(item => item.time_sec);
-            const createProbChart = (ctx, p_now, p_future, colors) => new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: time,
-                    datasets: [
-                        {
-                            label: 'Probability Now',
-                            data: p_now,
-                            borderColor: colors.now,
-                            fill: false,
-                            radius: 1
-                        },
-                        {
-                            label: 'Probability Future',
-                            data: p_future,
-                            borderColor: colors.future,
-                            fill: false,
-                            radius: 1
-                        }
-                    ]
-                },
-                options: {
-                    responsive: true,
-                    scales: {
-                        x: {
-                            type: 'linear',
-                            position: 'bottom',
-                            title: {
-                                display: true,
-                                text: 'Time (s)'
+            const time = data.map(item => parseFloat(item.time_sec));
+
+            const ctxNow = document.getElementById('probabilityChartNow').getContext('2d');
+            const ctxFuture = document.getElementById('probabilityChartFuture').getContext('2d');
+
+            const prepareProbData = (probData) => {
+                let above = [], below = [];
+                probData.forEach((value, index) => {
+                    if (value > 0.5) {
+                        above.push({ x: time[index], y: value });
+                        below.push({ x: time[index], y: 0.5 });
+                    } else {
+                        above.push({ x: time[index], y: 0.5 });
+                        below.push({ x: time[index], y: value });
+                    }
+                });
+                return { above, below };
+            };
+
+            const createProbChart = (ctx, probData, label) => {
+                const { above, below } = prepareProbData(probData);
+                return new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        datasets: [
+                            {
+                                label: `${label} (above 0.5)`,
+                                data: above,
+                                borderColor: 'rgba(255, 165, 0, 1)',
+                                backgroundColor: 'rgba(255, 165, 0, 0.5)',
+                                pointRadius: 0,
+                                fill: { target: { value: 0.5 }, above: 'rgba(255, 165, 0, 0.5)', below: 'rgba(255, 165, 0, 0.5)' },
+                                borderWidth: 1,
+                                segment: {
+                                    borderColor: ctx => ctx.p0.parsed.y > 0.5 ? 'rgba(255, 165, 0, 1)' : 'rgba(0, 0, 255, 1)',
+                                    backgroundColor: ctx => ctx.p0.parsed.y > 0.5 ? 'rgba(255, 165, 0, 0.5)' : 'rgba(0,0,255,0.5)'
+                                }
                             },
-                            min: 0,
-                            max: 0
-                        },
-                        y: {
-                            beginAtZero: true,
-                            title: {
-                                display: true,
-                                text: 'Probability'
+                            {
+                                label: `${label} (below 0.5)`,
+                                data: below,
+                                borderColor: 'rgba(0, 0, 255, 1)',
+                                backgroundColor: 'rgba(0, 0, 255, 0.5)',
+                                pointRadius: 0,
+                                fill: { target: { value: 0.5 }, above: 'rgba(0, 0, 255, 0.5)', below: 'rgba(0, 0, 255, 0.5)' },
+                                borderWidth: 1,
+                                segment: {
+                                    borderColor: ctx => ctx.p0.parsed.y > 0.5 ? 'rgba(255, 165, 0, 1)' : 'rgba(0, 0, 255, 1)',
+                                    backgroundColor: ctx => ctx.p0.parsed.y > 0.5 ? 'rgba(255, 165, 0, 0.5)' : 'rgba(0, 0, 255, 0.5)'
+                                }
                             }
-                        }
+                        ]
                     },
-                    plugins: {
-                        legend: {
-                            display: true,
-                            position: 'bottom',
-                            align: 'end'
+                    options: {
+                        responsive: true,
+                        scales: {
+                            x: {
+                                type: 'linear',
+                                position: 'bottom',
+                                title: {
+                                    display: true,
+                                    text: 'Time (s)'
+                                },
+                                min: 0,
+                                max: 0
+                            },
+                            y: {
+                                beginAtZero: true,
+                                title: {
+                                    display: true,
+                                    text: 'Probability'
+                                },
+                                min: 0,
+                                max: 1
+                            }
                         },
-                        annotation: {
-                            annotations: {
-                                line1: {
-                                    type: 'line',
-                                    borderColor: 'purple',
-                                    borderWidth: 2,
-                                    mode: 'vertical',
-                                    scaleID: 'x',
-                                    value: 0,
-                                    label: {
-                                        enabled: true,
-                                        content: 'Current Time',
-                                        position: 'top'
+                        plugins: {
+                            legend: {
+                                display: false,
+                            },
+                            annotation: {
+                                annotations: {
+                                    line1: {
+                                        type: 'line',
+                                        borderColor: 'purple',
+                                        borderWidth: 2,
+                                        mode: 'vertical',
+                                        scaleID: 'x',
+                                        value: 0,
+                                        label: {
+                                            enabled: true,
+                                            content: 'Current Time',
+                                            position: 'top'
+                                        }
                                     }
                                 }
                             }
                         }
-                    },
-                    onClick: (event, elements) => {
-                        if (elements.length > 0) {
-                            const element = elements[0];
-                            const time = chart.data.labels[element.index];
-                            const duration = wavesurfer.getDuration();
-                            wavesurfer.seekTo(time / duration);
-                        }
                     }
-                }
-            });
+                });
+            };
 
-            const ctxLeft = document.getElementById('probabilityChartLeft').getContext('2d');
-            const ctxRight = document.getElementById('probabilityChartRight').getContext('2d');
+            const probNow = data.map(item => parseFloat(item['p_now(0=left)']));
+            const probFuture = data.map(item => parseFloat(item['p_future(0=left)']));
 
-            const chartLeft = createProbChart(ctxLeft, data.map(item => item['p_now(0=left)']), data.map(item => item['p_future(0=left)']), { now: 'red', future: 'blue' });
-            const chartRight = createProbChart(ctxRight, data.map(item => item['p_now(1=right)']), data.map(item => item['p_future(1=right)']), { now: 'green', future: 'orange' });
+            const chartNow = createProbChart(ctxNow, probNow, 'Probability Now');
+            const chartFuture = createProbChart(ctxFuture, probFuture, 'Probability Future');
 
             const updateChartMax = (chart, wavesurfer) => {
                 const duration = wavesurfer.getDuration();
@@ -170,10 +179,37 @@ document.addEventListener('DOMContentLoaded', function () {
                 chart.update();
             };
 
-            wavesurferLeft.on('ready', () => updateChartMax(chartLeft, wavesurferLeft));
-            wavesurferRight.on('ready', () => updateChartMax(chartRight, wavesurferRight));
+            const updateAnnotation = (chart, time) => {
+                if (chart.options.plugins.annotation.annotations.line1) {
+                    chart.options.plugins.annotation.annotations.line1.value = time;
+                    chart.update('none');
+                }
+            };
+
+            const synchronizeSeek = (wavesurferFrom, wavesurferTo) => {
+                const currentTime = wavesurferFrom.getCurrentTime();
+                const duration = wavesurferTo.getDuration();
+                const isPlaying = wavesurferFrom.isPlaying();
+         
+                wavesurferTo.pause();
+                wavesurferTo.seekTo(currentTime / duration);
+                updateAnnotation(chartNow, currentTime);
+                updateAnnotation(chartFuture, currentTime);
+                if (isPlaying) {
+                    setTimeout(() => wavesurferTo.play(), 0);
+                }
+            };
+
+            wavesurferLeft.on('ready', () => updateChartMax(chartNow, wavesurferLeft));
+            wavesurferRight.on('ready', () => updateChartMax(chartFuture, wavesurferRight));
 
             wavesurferLeft.on('interaction', () => synchronizeSeek(wavesurferLeft, wavesurferRight));
             wavesurferRight.on('interaction', () => synchronizeSeek(wavesurferRight, wavesurferLeft));
+
+            wavesurferLeft.on('audioprocess', () => {
+                const currentTime = wavesurferLeft.getCurrentTime();
+                updateAnnotation(chartNow, currentTime);
+                updateAnnotation(chartFuture, currentTime);
+            });
         });
 });
